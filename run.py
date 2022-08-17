29ffdadc8aa7422214f338e924f55b62e87a8839
Lorenzo Santucci <lorenzosantucci@hotmail.it>
2022-08-17 09:45:45 +0000

    Set up the input key structure + an array of all winning combinations

--- run.py
+++ run.py
@@ -1,1 +1,26 @@
+from termcolor import colored
+from time import sleep
+import numpy as np
+import random
 
+# Initialize game field values
+game_keys = np.array([['q', 'w', 'e'],
+                      ['a', 's', 'd'],
+                      ['z', 'x', 'c']])
+
+# Detect all possible win combinations
+win_keys_combinations = np.array([game_keys[:, 0],
+                                  game_keys[:, 1],
+                                  game_keys[:, 2],
+                                  game_keys[0, :],
+                                  game_keys[1, :],
+                                  game_keys[2, :],
+                                  np.array([game_keys[0, 0],
+                                            game_keys[1, 1],
+                                            game_keys[2, 2]]),
+                                  np.array([game_keys[0, 2],
+                                            game_keys[1, 1],
+                                            game_keys[2, 0]])
+                                  ])
+
+print(game_keys, win_keys_combinations)
